99d4d951d00e8444d41ee68caeb3beefc3e2cadd
Adam Peer <adam0005@student.monash.edu>
2024-01-05 14:33:07 +0400

    Added Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from "@vercel/speed-insights/react"
 import { useState, useEffect } from 'react';
 import Lenis from '@studio-freight/lenis'
@@ -39,6 +40,7 @@ const App = () => {
           <CardList />
           <Contact />
           <SpeedInsights />
+          <Analytics />
         </>
       )}
     </>
